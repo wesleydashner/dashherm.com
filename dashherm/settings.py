a3ee01cd433a86d94a85efc8a3ba677229927f5c
Wesley Dashner <wdashner11@gmail.com>
2019-12-14 12:22:33 -0700

    everything working with default django page on dashherm.com

--- dashherm/settings.py
+++ dashherm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'mi+y4r%r-*-_77avp5lb2rifs%ung@b!zyljwcfn9nzw08w6@4'
 DEBUG = True
 
 # Do I need my public IP address here? It changes though...
-ALLOWED_HOSTS = ['dashherm.com', 'www.dashherm.com', '10.0.0.1', '10.0.1.1']
+ALLOWED_HOSTS = ['dashherm.com', 'www.dashherm.com', '10.0.0.1', '10.0.1.1', '24.10.132.46', '10.0.0.253']
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# Do I need to actually set BASE_DIR to something?
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
