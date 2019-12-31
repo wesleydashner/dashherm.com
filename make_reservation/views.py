8e9c665afff6a789760de96f6a35fe0b92d8c7f0
Wesley Dashner <wdashner11@gmail.com>
2019-12-31 10:23:36 -0700

    changed make-reservation code to properly decode data received

--- make_reservation/views.py
+++ make_reservation/views.py
@@ -1,12 +1,14 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from general_manager import GeneralManager
+import json
 
 
 @csrf_exempt
 def index(request):
-    lot_id = request.POST.get('lot_id')
-    user_id = request.POST.get('user_id')
+    data = json.loads(request.body.decode('utf-8'))
+    lot_id = data.get('lot_id')
+    user_id = data.get('user_id')
     if not lot_id or not user_id:
         return get_json_response(False)
     return get_json_response(GeneralManager.make_reservation(lot_id, user_id))
